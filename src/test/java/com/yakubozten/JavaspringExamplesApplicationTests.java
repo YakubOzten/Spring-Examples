d86ebdbf309b20d35510a2f92a3fed71e2c65f24
YakubOzten <yakubozten@gmail.com>
2024-07-18 21:26:29 +0300

    New Projects Started

---
+++ src/test/java/com/yakubozten/JavaspringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakubozten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
